e387529be66fcc391f35467879d84f602d4b31d1
Vlad Volchkov <145363900+srGOLD78@users.noreply.github.com>
2025-06-27 01:11:03 +0300

    Update 2.go

--- Hello/2.go
+++ Hello/2.go
@@ -8,7 +8,7 @@ var name, date string
 
 func main() {
 	name = "Владислав"
-	date = "20 июня 2025 год"
+	date = "27 июня 2025 год"
 
 	fmt.Println("Привет, меня зовут", name)
 	fmt.Println("Сегодня:", date)
